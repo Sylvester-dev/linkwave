4e350341cfc2d59627afa2d5e1803cfb911e8086
Vineet Kumar <vineetkumar@Vineets-MacBook-Air.local>
2023-09-16 20:21:44 +0530

    media room made

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,8 +2,9 @@ import { Hash } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
-
 import { SocketIndicator } from "@/components/socket-indicator";
+
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -12,7 +13,7 @@ interface ChatHeaderProps {
   imageUrl?: string;
 }
 
-export const  ChatHeader = ({
+export const ChatHeader = ({
   serverId,
   name,
   type,
@@ -34,6 +35,9 @@ export const  ChatHeader = ({
         {name}
       </p>
       <div className="ml-auto flex items-center">
+        {type === "conversation" && (
+          <ChatVideoButton />
+        )}
         <SocketIndicator />
       </div>
     </div>
